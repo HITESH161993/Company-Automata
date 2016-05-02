d2c95bdaf7882d99a767fc13407278463bcbee76
HITESH161993 <hitesh.garg.34.hg@gmail.com>
2016-05-03 00:56:15 +0530

    my first commit

---
+++ src/company/Company.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package company;
+
+/**
+ *
+ * @author HITESH
+ */
+public class Company {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
